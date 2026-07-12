71adebc5ee021acea0be0c1441ecc6f89c2b8cf2
Thu Kha Nay Win <thukhanaywin@gmail.com>
2026-07-12 10:56:06 +0630

    Add files via upload

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,7 +1,14 @@
+import os
+import json
+
 from firebase_admin import credentials
 import firebase_admin
 
-from app.core.config import FIREBASE_CREDENTIALS
 
-cred = credentials.Certificate(FIREBASE_CREDENTIALS)
+firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
+
+cred = credentials.Certificate(
+    json.loads(firebase_credentials)
+)
+
 firebase_admin.initialize_app(cred)
